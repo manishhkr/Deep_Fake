392118e57d877a895ee55e0c493d2c223cd260fb
manishhkr <130096475+manishhkr@users.noreply.github.com>
2024-09-20 22:41:37 +0530

    Update NEW_DF_API.py

--- NEW_DF_API.py
+++ NEW_DF_API.py
@@ -14,10 +14,15 @@ import os
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+# # Load text detection model and tokenizer
+# text_model_name = r"Deep_Fake"
+# text_model = AutoModelForSequenceClassification.from_pretrained(text_model_name)
+# text_tokenizer = AutoTokenizer.from_pretrained(text_model_name)
 # Load text detection model and tokenizer
-text_model_name = r"Deep_Fake"
+text_model_name = "Deep_Fake"  # This is the folder name which is also your repo name
 text_model = AutoModelForSequenceClassification.from_pretrained(text_model_name)
 text_tokenizer = AutoTokenizer.from_pretrained(text_model_name)
+
 
 # Video prediction model settings
 im_size = 112
